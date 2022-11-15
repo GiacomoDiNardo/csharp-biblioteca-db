8fca1fb8081f013258a90e47c024cd30cb2ecb9c
GiacomoDiNardo <vest_95@hotmail.it>
2022-11-15 15:42:21 +0100

    insert and select query

---
+++ Documento.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+public class Documento
+{
+    public int Codice { get; set; }
+    public string Titolo { get; set; }
+    public int Anno { get; set; }
+    public string Settore { get; set; }
+    public bool Stato { get; set; }
+    public int Scaffale { get; set; }
+    public string Autore { get; set; }
+
+    public Documento(int codice, string titolo, int anno, string settore, bool stato, int scaffale, string autore)
+    {
+        Codice = codice;
+        Titolo = titolo;
+        Anno = anno;
+        Settore = settore;
+        Stato = stato;
+        Scaffale = scaffale;
+        Autore = autore;
+    }
+
+    public override string ToString()
+    {
+        return "DC" + Codice;
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,91 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using System.Data.SqlClient;
+
+string stringaConnessione = "Data Source=localhost;Initial Catalog=biblioteca-db;Integrated Security=True";
+
+SqlConnection connessioneSql = new SqlConnection(stringaConnessione);
+
+//try
+//{
+//    connessioneSql.Open();
+
+//    string insertQuery = "INSERT INTO documenti (codice, titolo, anno, settore, stato, scaffale, autore, tipo) VALUES (@codice, @titolo, @anno, @settore, @stato, @scaffale, @autore, @tipo)";
+
+//    SqlCommand insertCommand = new SqlCommand(insertQuery, connessioneSql);
+
+//    int codice = new Random().Next(10000, 100000);
+//    Console.WriteLine("inserisci titolo documento");
+//    string titolo = Console.ReadLine();
+//    Console.WriteLine("inserisci anno di pubblicazione");
+//    DateTime anno = Convert.ToDateTime(Console.ReadLine());
+//    Console.WriteLine("inserisci settore documento");
+//    string settore = Console.ReadLine();
+//    Console.WriteLine("inserisci scaffale");
+//    int scaffale = Convert.ToInt32(Console.ReadLine());
+//    Console.WriteLine("inserisci autore documento");
+//    string autore = Console.ReadLine();
+//    Console.WriteLine("inserisci tipo di documento");
+//    string tipo = Console.ReadLine();
+
+//    insertCommand.Parameters.Add(new SqlParameter("@codice", codice));
+//    insertCommand.Parameters.Add(new SqlParameter("@titolo", titolo));
+//    insertCommand.Parameters.Add(new SqlParameter("@anno", anno));
+//    insertCommand.Parameters.Add(new SqlParameter("@settore", settore));
+//    insertCommand.Parameters.Add(new SqlParameter("@stato", true));
+//    insertCommand.Parameters.Add(new SqlParameter("@scaffale", scaffale));
+//    insertCommand.Parameters.Add(new SqlParameter("@autore", autore));
+//    insertCommand.Parameters.Add(new SqlParameter("@tipo", tipo));
+
+//    int affectedRows = insertCommand.ExecuteNonQuery();
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//} finally
+//{
+//    connessioneSql.Close();
+//}
+
+
+try
+{
+    connessioneSql.Open();
+
+    Console.WriteLine("inserisci il titolo del documento:");
+    string titolo = Console.ReadLine();
+
+    string query = "SELECT * FROM documenti where titolo=@titolo";
+
+    // creo il comando ed eseguo la query
+    SqlCommand cmd = new SqlCommand(query, connessioneSql);
+    cmd.Parameters.Add(new SqlParameter("@titolo", titolo));
+
+    //reader 
+    SqlDataReader reader = cmd.ExecuteReader();
+
+    while (reader.Read())
+    {
+        int codice = reader.GetInt32(1);
+        Console.WriteLine(codice);
+        string nome = reader.GetString(2);
+        Console.WriteLine(nome);
+        string autore = reader.GetString(7);
+        Console.WriteLine(autore);
+        bool disponibile = reader.GetBoolean(5);
+        Console.WriteLine(disponibile == true ? "disponibile" : "non disponibile");
+        int scaffale = reader.GetInt32(6);
+        Console.WriteLine(scaffale);
+    }
+
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    connessioneSql.Close();
+}
